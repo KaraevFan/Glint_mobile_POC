e08678455a324ca5f8f14cacc57d3635f9d08dfa
KaraevFan <tom.n.kano@gmail.com>
2025-05-02 15:19:05 +0900

    Initial Commit

---
+++ Glint_mobile_POC/Glint_mobile_POCApp.swift
@@ -0,0 +1,17 @@
+//
+//  Glint_mobile_POCApp.swift
+//  Glint_mobile_POC
+//
+//  Created by Tomoyuki Kano on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Glint_mobile_POCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Glint_mobile_POCTests/Glint_mobile_POCTests.swift
@@ -0,0 +1,17 @@
+//
+//  Glint_mobile_POCTests.swift
+//  Glint_mobile_POCTests
+//
+//  Created by Tomoyuki Kano on 5/2/25.
+//
+
+import Testing
+@testable import Glint_mobile_POC
+
+struct Glint_mobile_POCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
